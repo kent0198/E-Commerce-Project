fd5571538a76555b752e30a7780f702310c7445c
Kem <tuetran25112002@gmail.com>
2023-09-04 19:06:03 +0700

    not fix

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import slide from '../assets/slide.png'
+import lottie from 'lottie-web'
+
 const Banner = () => {
+
+  const container= useRef(null)
+
+  useEffect(()=>{
+    lottie.loadAnimation({
+      container:container.current,
+      renderer:'svg',
+      loop:true,
+      autoplay:true,
+      controls:true,
+      direction:-1,
+      animationData:require('./Canvas/hamter.json')
+    },[]) 
+  })
   return (
-    <div className='w-full'>
-        <img src={slide} alt='banner' className='h-[400px] object-cover w-full rounded-lg'/>
+    <div className='w-full '>
+        <div className='h-16' ref={container}></div>
     </div>
   )
 }
